47041c574b503cd4bad2c7a219e49940855d7ccb
imguest24897-alt <161818573+imguest24897-alt@users.noreply.github.com>
2025-01-29 17:42:18 +0200

    added the npm start thingy

---
+++ npm-scripts/start.js
@@ -0,0 +1,6 @@
+console.log("===============================================");
+console.log("            FNFSock v0.0.1 Beta                ");
+console.log("===============================================");
+console.log("Starting server...");
+console.warn("W: not implemented yet, sorry but i am working on it");
+console.log("Exiting...");
